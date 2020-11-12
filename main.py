8005a73a90b72689468aa4780636bca84ca6a915
Rudywasnthere <rudybcchs@gmail.com>
2020-11-12 17:02:02 +0000

    maybe works on command line now?

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 
 import math, sys
 import random, sys
-import time, sys
 
-non_prefixed_number= 0
 ##introdcution
 print("Hi I am here to help calculate the resistance for you")
 print("First I will need the colors on the resistor:\n either: black, brown, red, orange, yellow, green, blue, violet(purple), gold, silver, or transparent(none)")
+
 ## dictionaries for reference in my functions
 possible_first_colors = ["black","brown","red","orange","yellow","green","blue","violet","purple",""]
 possible_fourth_colors = ["gold","silver","transparent","none"]
@@ -16,8 +15,9 @@ color_values= {'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue
 tolerance_values= {'gold':0.05,'silver':0.1,'transparent':0.2,'none':0.2}
 tolerance_percents = {'gold':5, 'silver':10, 'transparent':20, 'none':20}
 count_prefixes= {'0':"", '1':'K','2':'M','3':'G','4':'T','5':'P','6':'E'}
+
 ##funcitons that sorts inputs, and calculate based on them
-## calculates upper and lower tolerance values based on fourth color band, and the nominal value
+## ensures correct initial inputs
 def correct_input(name_check):
   position = name_check.split("_")
   true_position = position.pop(0)
@@ -31,11 +31,13 @@ def correct_input(name_check):
       name = str(input("Please input a correct fourth color: "))
   return name
 
+##calculates un simplified nominal value
 def calculations(first_color, second_color, third_color): 
   first_digit= int(color_values[f'{first_color}'])*10 + int(color_values[f'{second_color}'])
   non_Prefixed_number= int(first_digit)*(10**int(color_values[f'{third_color}']))
   return non_Prefixed_number
 
+## calculates the prefix and simplified number for engineering notation
 def prefix_calculator(non_prefixed_number):
   count= 0 
   while non_prefixed_number>=1000:
@@ -47,6 +49,7 @@ def prefix_calculator(non_prefixed_number):
   prefixed_number = f"{non_prefixed_number} {prefix}"
   return prefixed_number, count, prefix, simplified_number
 
+##calculates the upper and lower limits for tolerance
 def tolerance(fourth_color, count, nominal_value):
   variance = tolerance_values[f'{fourth_color}']
   variance_percent = variance*100
@@ -62,6 +65,7 @@ def tolerance(fourth_color, count, nominal_value):
   upper_nominal= float(nominal_value + number_variance)
   return lower_prefix, lower_nominal, upper_nominal, variance_percent
 
+##rounds the outputs according to the user's desires
 def round_outputs(upper, lower, non_prefixed_number):
   length_lower = len(str(non_prefixed_number))
   f= False
@@ -88,15 +92,18 @@ def round_outputs(upper, lower, non_prefixed_number):
       final_lower= round(lower,round_place)
       final_upper= round(upper,round_place)
   return final_number, final_lower, final_upper, round_places
-## gets the final outputs to give to the user
 
+## gets the final outputs to give to the user
 def final_output(prefixed_number, non_prefixed_number, final_lower, final_upper, low_prefix, prefix):   
   if low_prefix =="":
    return f"\nnominal value= {prefixed_number} Ohms\n minimum= {final_lower} {prefix} Ohms\n maximum=  {final_upper} {prefix} Ohms"
   if low_prefix !="":
     return f"\nnominal value= {prefixed_number} Ohms\n minimum= {final_lower} {low_prefix} Ohms\n maximum=  {final_upper} {prefix} Ohms"
 
-def main():
+## main function for fun (possibly)
+
+restart= ""
+while restart!="done":
   first_color = correct_input("first_color")
   second_color = correct_input("second_color")
   third_color = correct_input("third_color")
@@ -111,8 +118,4 @@ def main():
   final_number, final_lower, final_upper, round_places = round_outputs(upper, lower, non_prefixed_number)
   print(F"Your color inputs are {first_color}, {second_color}, {third_color} ({non_prefixed_number}) and your tolerance is ±{variance_percent}% and you rounded to {round_places} places ")
   print(final_output(prefixed_number, non_prefixed_number, final_lower, final_upper, low_prefix, prefix))
-
-restart= ""
-while restart!="done":
-  main()
   restart = input("\nderp ☉ ‿ ⚆\n\n press any button to continue, enter \"done\" when your finished to close the file.\n")
